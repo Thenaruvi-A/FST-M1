bc7271ea65d00c53de896e36f9d4c2d6b979641d
Thenaruvi-A <asa.arumugam@gmail.com>
2025-07-27 19:08:54 +0530

    Add files via upload

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,13 @@
+package activities;
+
+public class Activity5 {
+	
+	public static void main(String[] args) {
+		Mybook newNovel = new Mybook();
+		newNovel.setTitle("The Elephant Tree");
+		System.out.println("The book is: " + newNovel.getTitle());
+	}
+	
+        
+        }
+          
---
+++ Java/Activities/Mybook.java
@@ -0,0 +1,8 @@
+package activities;
+
+public class Mybook extends Book{
+	
+	public void setTitle(String s) {
+	    title = s;
+	}
+}     
